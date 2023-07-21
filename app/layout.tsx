935339d1138d4866775c8cb2c4e5ff5994432b1d
sam <ironkwec@myumanitoba.ca>
2023-07-21 02:27:18 -0500

    e

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,12 @@ import { PropsWithChildren } from 'react';
 import 'styles/main.css';
 
 const meta = {
-  title: 'Next.js Subscription Starter',
-  description: 'Brought to you by Vercel, Stripe, and Supabase.',
+  title: 'Fitpal AI',
+  description: 'Meal plans and calorie tracking.',
   cardImage: '/og.png',
   robots: 'follow, index',
   favicon: '/favicon.ico',
-  url: 'https://subscription-starter.vercel.app',
+  url: 'https://fitpalai.com',
   type: 'website'
 };
 
