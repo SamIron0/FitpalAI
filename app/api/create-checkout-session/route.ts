6ae9e57b2ff97bdb5574298ed37eab484f9bc668
sam <ironkwec@myumanitoba.ca>
2023-07-20 22:19:53 -0500

    s

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -111,7 +111,7 @@ export async function POST(req: Request) {
     
     // 1. Destructure the price and quantity from the POST body
     const { queryText } = await req.json();
-    const customer = await createOrRetrieveMealPlan(testPlan, "rtr", "string", "string" );
+    //const customer = await createOrRetrieveMealPlan(testPlan, "rtr", "string", "string" );
     /*const nb = await createOrRetrieveCustomer({
       uuid:  '',
       email: ''
