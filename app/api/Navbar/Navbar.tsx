1c1db972000e82ee518291c0a9009f2884ab2f2a
sam <ironkwec@myumanitoba.ca>
2023-07-20 22:46:48 -0500

    s

--- app/api/Navbar/Navbar.tsx
+++ app/api/Navbar/Navbar.tsx
@@ -3,7 +3,6 @@ import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 import Logo from '@/components/icons/Logo';
 import { useUser } from '@/utils/useUser';
-
 import s from './Navbar.module.css';
 //import Button from '../Button/Button';
 //import { UserMenu } from '@/components/user-menu';
