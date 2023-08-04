0c86d401e03b1cee4409ff8d9e5d02b7c53a9dff
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-04 14:24:56 -0600

    re

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -8,7 +8,6 @@ const Logo = ({ className = '', ...props }) => (
     className={className}
     {...props}
   >
-
     <rect width="100%" height="100%" rx="16" fill="white" />
     <path
       fillRule="evenodd"
