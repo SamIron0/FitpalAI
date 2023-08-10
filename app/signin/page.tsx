8c359c4ca1a11371682d7b97b49e08812159d408
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-10 22:58:02 +0000

    login

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -6,18 +6,19 @@ import Logo from '@/components/icons/Logo';
 import logo from "../../../logo.png";
 
 export default async function SignIn() {
-  const session = await getSession();
-  if (session) {
-    const user = session?.user;
-    return redirect(`https://chat.fitpalai.com`);
-  }
-
-  return (
-    <div className="flex justify-center height-screen-helper">
-      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
-
-        <AuthUI />
+  //const session = await getSession();
+  //if (session) {
+  //const user = session?.user;
+  redirect(`https://chat.fitpalai.com`);
+  // }
+  /*
+    return (
+      <div className="flex justify-center height-screen-helper">
+        <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
+  
+          <AuthUI />
+        </div>
       </div>
-    </div>
-  );
+    );
+    */
 }
