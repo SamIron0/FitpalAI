46a59b1feefc860b962b46d0a08673d518e97d1e
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-13 18:02:57 +0000

    sd

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -74,6 +74,7 @@ function waitlist() {
               // Handle error here
             })}
 
+            
             className="group mt-8 w-[310px] h-[50px] md:w-[500px] rounded-md px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
             style={{
               boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
