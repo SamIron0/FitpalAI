ec658a20c51940e5fc693b0f1a9f281527fd5f60
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-13 18:51:10 +0000

    j

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -10,7 +10,7 @@ import logo from "../../../logo.png";
 import Image from "next/image";
 
 export default async function Navbar() {
-  //const router = useRouter();
+  const router = useRouter();
 
   //const supabase = createServerSupabaseClient();
   //const {
@@ -37,8 +37,15 @@ export default async function Navbar() {
 
 
           <div className="flex flex-1 items-center justify-end space-x-8">
-            
+            <Link
+              href="/signin"
+              className="group mt-8 rounded-md px-4 py-2 text-[13px] font-semibold transition-all items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
+              style={{
+                boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
+              }}
+            >
               Get Started
+            </Link>
           </div>
         </div>
       </div>
