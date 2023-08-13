2243199869be227241ccb99ef90ceb748e0f054a
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-13 18:28:30 +0000

    signin

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
 import Link from 'next/link';
 import { createServerSupabaseClient } from '@/app/supabase-server';
 
@@ -8,6 +11,8 @@ import logo from "../../../logo.png";
 import Image from "next/image";
 
 export default async function Navbar() {
+  const router = useRouter();
+
   const supabase = createServerSupabaseClient();
   const {
     data: { user }
@@ -35,13 +40,13 @@ export default async function Navbar() {
           <div className="flex flex-1 items-center justify-end space-x-8">
             <button
               type="button"
-
-              className="group mt-8 w-[310px] h-[50px] md:w-[500px] rounded-md px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
+              onClick={router.push('/signin')}
+              className="group mt-8 rounded-md px-4 py-2 text-[13px] font-semibold transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
               style={{
                 boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
               }}
             >
-              <Link href="/signin" className="active:scale-95 items-center cursor-pointer text-zinc-200 rounded-md p-1 font-medium scale-100 duration-75">
+              <Link href="/signin">
                 Get Started
               </Link>
 
