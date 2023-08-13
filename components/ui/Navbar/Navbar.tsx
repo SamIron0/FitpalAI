d89f9ad3622e83e8c474b27a008e48ec6cd558bc
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-13 16:47:26 +0000

    ddf

--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -35,7 +35,6 @@ export default async function Navbar() {
 
 
           <button
-
             className="group mt-8 rounded-md px-4 py-2 text-[10px] transition-all flex items-center justify-center bg-[#f5f7f9] text-[#1E2B3A] no-underline active:scale-95 scale-100 duration-75"
             style={{
               boxShadow: "0 1px 1px #0c192714, 0 1px 3px #0c192724",
@@ -43,9 +42,7 @@ export default async function Navbar() {
           >
             <Link href="/signin">
               Get Started
-
             </Link>
-
           </button>
         </div>
       </div>
