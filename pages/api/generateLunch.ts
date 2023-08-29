e1c39691ca9a7b78b53aa819651650f0cd180859
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-28 22:57:33 -0500

    cv

--- pages/api/generateLunch.ts
+++ pages/api/generateLunch.ts
@@ -12,7 +12,7 @@ const handler: NextApiHandler = async (req, res) => {
     }
 
     const { calories, ingredients, userLocation, allergies } = req.query;
-    const userQuery = calories>0 ? `Could you please provide a detailed lunch recipe title utilising ${ingredients} that falls within ${calories} calories? The meals should reflect a variety of cuisines and flavor profiles suitable for a user based in ${userLocation}. Also, ensure that each meal offers balanced nutrients. Kindly deliver the response in the following JSON format: { {title: string}, ingredients: string })`
+    const userQuery = calories ? `Could you please provide a detailed lunch recipe title utilising ${ingredients} that falls within ${calories} calories? The meals should reflect a variety of cuisines and flavor profiles suitable for a user based in ${userLocation}. Also, ensure that each meal offers balanced nutrients. Kindly deliver the response in the following JSON format: { {title: string}, ingredients: string })`
         : `Could you please provide a detailed lunch recipe title utilising ${ingredients}? The meals should reflect a variety of cuisines and flavor profiles suitable for a user based in ${userLocation}. Also, ensure that each meal offers balanced nutrients. Kindly deliver the response in the following JSON format: { {title: string}, ingredients: string })`
     try {
         const chatResponse = await openai.chat.completions.create({
