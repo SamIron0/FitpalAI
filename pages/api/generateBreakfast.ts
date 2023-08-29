82b9b02c34f1b0eb9fd9dd201f3cc4f43c271277
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-28 21:55:17 -0500

    er

--- pages/api/generateBreakfast.ts
+++ pages/api/generateBreakfast.ts
@@ -1,8 +1,10 @@
 
 import { NextApiHandler } from 'next'
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
-const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_API_KEY }));
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
 const handler: NextApiHandler = async (req, res) => {
     if (req.method !== 'GET') {
@@ -14,11 +16,12 @@ const handler: NextApiHandler = async (req, res) => {
     const userQuery = calories ? `Could you please provide a detailed lunch recipe title utilising ${ingredients} that falls within ${calories} calories? The meals should reflect a variety of cuisines and flavor profiles suitable for a user based in ${userLocation}. Also, ensure that each meal offers balanced nutrients. Kindly deliver the response in the following JSON format: { {title: string}, ingredients: string })`
         : `Could you please provide a detailed lunch recipe title utilising ${ingredients}? The meals should reflect a variety of cuisines and flavor profiles suitable for a user based in ${userLocation}. Also, ensure that each meal offers balanced nutrients. Kindly deliver the response in the following JSON format: { {title: string}, ingredients: string })`
     try {
-        const chatResponse = await openai.createChatCompletion({
+        const chatResponse = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: userQuery }],
         });
-        const text = chatResponse.data.choices[0].message?.content;
+        const text = chatCompletion.data.choices[0].message.content;
+        //const text = chatResponse.data.choices[0].message?.content;
 
         if (typeof text !== 'string') {
             return res.status(500).json({ message: 'API Error' });
