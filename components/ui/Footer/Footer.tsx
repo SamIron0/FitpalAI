afbe19e60d6cc75ef0eb119aea6545281a085498
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-31 17:17:07 -0500

    i

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -6,18 +6,7 @@ import GitHub from '@/components/icons/GitHub';
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-[1920px] px-6 bg-zinc-900">
-      <div className="grid grid-cols-1 gap-8 py-12 text-white transition-colors duration-150 border-b lg:grid-cols-12 border-zinc-600 bg-zinc-900">
-        <div className="flex items-start col-span-1 text-white lg:col-span-6 lg:justify-end">
-          <div className="flex items-center h-10 space-x-6">
-            <a
-              aria-label="Github Repository"
-              href="https://github.com/vercel/nextjs-subscription-payments"
-            >
-              <GitHub />
-            </a>
-          </div>
-        </div>
-      </div>
+     
       <div className="flex flex-col items-center justify-between py-12 space-y-4 md:flex-row bg-zinc-900">
         <div>
           <span>
