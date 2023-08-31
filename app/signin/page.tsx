cb2c9df8c3b6e69540060716bfb48400a6b903f9
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-31 18:18:22 -0500

    d

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,7 +2,7 @@ import { getSession } from '@/app/supabase-server';
 import AuthUI from './AuthUI';
 import { redirect } from 'next/navigation';
 import Logo from '@/components/icons/Logo';
-import logo from "../../../logo.png";
+//import logo from "../../../logo.png";
 
 export default async function SignIn() {
   const session = await getSession();
@@ -16,9 +16,9 @@ export default async function SignIn() {
     <div className="flex justify-center height-screen-helper">
       <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
         <img
-          src={logo.src}
+          src="/logo.svg"
            alt="logo"
-           className='h-12 mb-9 '
+           className='h-18 mb-9 '
           ></img>
         <AuthUI />
       </div>
