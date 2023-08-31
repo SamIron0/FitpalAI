6fbc057643b4d77a3b303df871a2f1a22c3409a9
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-31 17:38:57 -0500

    k

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -7,18 +7,17 @@ export default function Footer() {
   return (
     <footer className="mx-auto max-w-[1920px] px-6 bg-zinc-900">
      
-      <div className="flex flex-col items-center justify-between py-12 space-y-4 md:flex-row bg-zinc-900">
+      <div className="flex flex-col items-center justify-between py-6 space-y-4 flex-row bg-zinc-900">
         <div>
           <span>
-            &copy; {new Date().getFullYear()} ACME, Inc. All rights reserved.
-          </span>
+            &copy; {new Date().getFullYear()} FitpalAI. </span>
         </div>
         <div className="flex items-center">
           <a href="https://twitter.com" aria-label="twitter link">
             <img
               src="/x-logo.svg"
               alt="twitter"
-              className="inline-block h-6 ml-4 text-white"
+              className="inline-block h-8 text-white"
             />
           </a>
         </div>
