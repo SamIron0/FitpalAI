64a1aeb67bd92dfae72a8fb273952f3ba49a3c1c
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-31 19:00:03 -0500

    e

--- pages/api/generateBreakfast.ts
+++ pages/api/generateBreakfast.ts
@@ -11,10 +11,8 @@ const handler: NextApiHandler = async (req, res) => {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
     const { calories, ingredients, userLocation, allergies } = req.query;
-    const userQuery = calories ? `Step 1: I have ${ingredients}  in my pantry but im very willing to buy more ingredients, out of those, select only the ones most commonly used to make breakfast. Step 2: Please select a common popular breakfast recipe with ${calories} based on already existing cookbook articles that may or may not contain some of the pantry ingredients, Staying within a 5 calorie range of the total. Deliver in JSON format: {title: string; ingredients: string; calories: number}`
-       
-        : `Could you please provide a detailed breakfast recipe title optionally utilising ${ingredients}? The meals should reflect a variety of cuisines and flavor profiles suitable for a user based in ${userLocation}. Kindly deliver the response in the following JSON format: {title: string;
-            ingredients: string;calories: number}`
+    const userQuery = calories ? `Step 1: I have ${ingredients}  in my pantry but im very willing to buy more ingredients, out of those, select only the ones most commonly used to make breakfast. Step 2: Please select a common popular breakfast recipe with ${calories} calories based on already existing cookbook articles that may or may not contain some of the pantry ingredients, Staying within a 5 calorie range of the total. Deliver in JSON format: {title: string; ingredients: string; calories: number}`
+        : `Step 1: I have ${ingredients}  in my pantry but im very willing to buy more ingredients, out of those, select only the ones most commonly used to make breakfast. Step 2: Please select a common popular breakfast recipe based on already existing cookbook articles that may or may not contain some of the pantry ingredients. Deliver in JSON format: {title: string; ingredients: string; calories: number}`
     try {
         const chatResponse = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
