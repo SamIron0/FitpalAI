e636b42a878fc3223885fa7ed1d86c9776a2d256
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-08-31 23:33:23 -0500

    d

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
           <span>
             &copy; {new Date().getFullYear()} FitpalAI. </span>
         </div>
-        <div className="">
+        <div className="flex items-center">
           <a className="" href="https://twitter.com" aria-label="twitter link">
             <img
               src="/x-logo.svg"
