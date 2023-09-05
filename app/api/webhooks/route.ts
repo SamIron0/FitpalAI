11dd5faa2c51b410fbefd25d024c95e77b394ae8
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-04 23:35:57 -0500

    x

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -20,7 +20,7 @@ const relevantEvents = new Set([
 ]);
 export async function POST(req: Request) {
   if (req) {
-    const body = await req?.body.text();
+    const body = await req?.body?.text();
     const sig = headers().get('Stripe-Signature') as string;
     const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
     let event: Stripe.Event;
