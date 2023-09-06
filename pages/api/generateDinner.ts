11b4e29ed2f59c9aad444a87c515d058682b44b0
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-05 21:01:00 -0500

    s

--- pages/api/generateDinner.ts
+++ pages/api/generateDinner.ts
@@ -11,28 +11,44 @@ const handler: NextApiHandler = async (req, res) => {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
     const { calories, ingredients, userLocation, allergy } = req.query;
-    const userQuery = calories ? ingredients != "" ? allergy != "" ? `I have ${ingredients} in my pantry, out of those, note only the ones most commonly used to make dinner. Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total. . Do not include anything that contains ${allergy}.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string;  calories: number}`
-        : `Step 1: I have ${ingredients}  in my pantry, out of those, select only the ones most commonly used to make dinner. Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total. Deliver in JSON format: {title: string;  calories: number}`
-        : `Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles, Staying within a 5 calorie range of the total. Deliver in JSON format: {title: string; calories: number}`
-        : calories ? allergy != "" ? `Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total. Do not include anything that contains ${allergy}.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string;  calories: number}`
-            : `Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles, Staying within a 5 calorie range of the total. Deliver in JSON format: {title: string; calories: number}`
-            : `Please select one common popular dinner recipe based on already existing cookbook articles. Deliver in JSON format: {title: string; calories: number}`
+    let userQuery;
 
-    try {
-        const chatResponse = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: userQuery }],
-        });
-        //const text = chatCompletion.data.choices[0].message.content;
-        const text = chatResponse.choices[0].message.content;
+    if (calories && ingredients !== "" && allergy !== "") {
+        userQuery = `I have ${ingredients} in my pantry, out of those, note only the ones most commonly used to make dinner. Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total. Do not include anything that contains ${allergy}.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string; calories: number}`;
+    } else if (calories && ingredients !== "") {
+        userQuery = `Step 1: I have ${ingredients}  in my pantry, out of those, select only the ones most commonly used to make dinner. Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total. Deliver in JSON format: {title: string; calories: number}`;
+    } else if (calories && allergy !== "") {
+        userQuery = ` Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total. Do not include anything that contains ${allergy}.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string; calories: number}`;
+    }else if (allergy !== "" && ingredients !== "") {
+        userQuery = ` I have ${ingredients}  in my pantry, out of those, select only the ones most commonly used to make dinner. Please select one common popular dinner recipe based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe. Do not include anything that contains ${allergy}.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string; calories: number}`;
+    }else if (allergy !== "" ) {
+        userQuery = ` Please select one common popular dinner recipe based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe. Do not include anything that contains ${allergy}.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string; calories: number}`;
+    } else if (ingredients !== "") {
+        userQuery = ` I have ${ingredients}  in my pantry, out of those, select only the ones most commonly used to make dinner. Please select one common popular dinner recipe that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string; calories: number}`;
 
-        if (typeof text !== 'string') {
-            return res.status(500).json({ message: 'API Error' });
-        }
-
-        return res.status(200).json({ "meal": JSON.parse(text) });
-    } catch (error) {
     }
+    else if (calories) {
+        userQuery = ` Please select one common popular dinner recipe with ${calories} calories based on already existing cookbook articles that may or may not have some of the pantry ingredients listed as an ingredient of the dinner recipe, Staying within a 5 calorie range of the total.The person cooking is located in ${userLocation}. Deliver in JSON format: {title: string; calories: number}`;
+
+    }
+    
+
+
+try {
+    const chatResponse = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: userQuery }],
+    });
+    //const text = chatCompletion.data.choices[0].message.content;
+    const text = chatResponse.choices[0].message.content;
+
+    if (typeof text !== 'string') {
+        return res.status(500).json({ message: 'API Error' });
+    }
+
+    return res.status(200).json({ "meal": JSON.parse(text) });
+} catch (error) {
+}
 }
 
 export default handler;
