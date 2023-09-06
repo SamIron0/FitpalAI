1b77b3985af2c2274e68cc8430b9049ab1f71a0e
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-06 12:48:37 -0500

    s

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -162,6 +162,7 @@ export default function Pricing({
       </section>
     );
 
+    
   return (
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
