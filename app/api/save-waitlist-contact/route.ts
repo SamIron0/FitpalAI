9af4a8afb58a646c8875ee47729a706df77e62ab
SamIron0 <127967090+SamIron0@users.noreply.github.com>
2023-09-20 21:19:33 -0500

    3

--- app/api/save-waitlist-contact/route.ts
+++ app/api/save-waitlist-contact/route.ts
@@ -1,19 +1,12 @@
 import { cookies, headers } from "next/headers";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { stripe } from "@/utils/stripe";
-import {
-  createOrRetrieveCustomer,
-  createOrRetrieveMealPlan,
-  createOrRetrieveWaitListContact,
-} from "@/utils/supabase-admin";
+import { createOrRetrieveWaitListContact } from "@/utils/supabase-admin";
 import { getURL } from "@/utils/helpers";
 import { Database } from "@/types_db";
-import { MealPlan } from "@/types";
 
 export async function POST(req: Request) {
   if (req.method === "POST") {
     try {
-      // 1. Destructure the price and quantity from the POST body
       const { userName, userEmail } = await req.json();
       const response = await createOrRetrieveWaitListContact(
         userName,
