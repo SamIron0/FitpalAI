0770a7ad9402cd5ba45585455d92f407b5de0e86
sam <samuelironkwec@gmail.com>
2024-01-21 19:46:55 -0600

    x

--- app/page.tsx
+++ app/page.tsx
@@ -62,15 +62,29 @@ export default async function Home() {
               </Link>
             </div>
           </div>
-          <div className="w-full p-24 flex justify-center items-center">
-            <div className="rounded-lg overflow-hidden w-full border-[1px] border-zinc-600">
-              <img src={'/mockup1.png'} alt="mockup image" />
-            </div>
-          </div>
-          <div>
-            </ div>
-          <Footer />
+            <div className="w-full px-24 flex justify-center items-center">
+              <figure className="border-1 border-zinc-600 relative z-[1] max-w-full w-[50rem] h-auto shadow-[0_2.75rem_3.5rem_-2rem_rgb(45_55_75_/_20%),_0_0_5rem_-2rem_rgb(45_55_75_/_15%)] dark:shadow-[0_2.75rem_3.5rem_-2rem_rgb(0_0_0_/_20%),_0_0_5rem_-2rem_rgb(0_0_0_/_15%)]">
+                <div className="relative flex items-center max-w-[50rem] bg-gray-800 rounded-t-lg py-2 px-24 dark:bg-gray-700">
+                  <div className="flex space-x-1 absolute top-2/4 start-4 -translate-y-1">
+                    <span className="w-2 h-2 bg-gray-600 rounded-full dark:bg-gray-600"></span>
+                    <span className="w-2 h-2 bg-gray-600 rounded-full dark:bg-gray-600"></span>
+                    <span className="w-2 h-2 bg-gray-600 rounded-full dark:bg-gray-600"></span>
+                  </div>
+                  <div className="flex justify-center items-center w-full h-full bg-gray-700 text-[.25rem] text-gray-400 rounded-sm sm:text-[.5rem] dark:bg-gray-600 dark:text-gray-400">
+                    www.fitpalai.com
+                  </div>
+                </div>
 
+                <div className="bg-gray-800 rounded-b-lg">
+                  <img
+                    className="max-w-full h-auto rounded-b-lg"
+                    src="mockup1.png"
+                    alt="ImageMockup"
+                  />
+                </div>
+              </figure>
+            </div>
+          <Footer />
         </div>
       </>
     );
