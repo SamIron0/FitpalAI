97c8368d8c9e28d7721352799394bd1dee7e22d4
sam <samuelironkwec@gmail.com>
2024-01-23 13:53:56 -0600

    x

--- pages/api/send-waitlist-email.ts
+++ pages/api/send-waitlist-email.ts
@@ -4,17 +4,26 @@ import { Resend } from 'resend';
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const { name, email } = req.body;
+    const { name, email } = req.query;
     const resend = new Resend('re_N34y4JVC_51XbLjazeSGG4cLdn1HjsFtn');
-    resend.emails.send({
+
+    const toEmail = email ? String(email) : ''; // Ensure toEmail is always a string
+    const userName = name ? String(name) : ''; // Ensure toEmail is always a string
+
+    await resend.emails.send({
       from: 'FitpalAI <samuel@fitpalai.com>',
-      to: [email],
+      to: toEmail,
       subject: 'Thank you for joining the fitpal community!',
-      react: WaitlistEmailTemplate(name)
+      react: WaitlistEmailTemplate(userName)
     });
-    //console.log(result);
+
+    res
+      .status(200)
+      .json({ success: true, message: 'Email sent successfully.' });
   } catch (error) {
-    console.log(error);
+    console.error('Error handling request:', error);
+    res.status(500).json({ success: false, error: 'Internal server error.' });
   }
 };
+
 export default handler;
