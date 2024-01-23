015974b0b1d641ae33ad65e8cb0811b816676a53
sam <samuelironkwec@gmail.com>
2024-01-23 13:21:56 -0600

    x

--- pages/api/send-waitlist-email.ts
+++ pages/api/send-waitlist-email.ts
@@ -6,8 +6,8 @@ const handler: NextApiHandler = async (req, res) => {
   try {
     const resend = new Resend('re_N34y4JVC_51XbLjazeSGG4cLdn1HjsFtn');
     resend.emails.send({
-      from: 'samuel@fitpalai.com',
-      to: 'samuelironkwec@gmail.com',
+      from: 'FitpalAI <samuel@fitpalai.com>',
+      to: ['samuelironkwec@gmail.com'],
       subject: 'Thank you for joining the fitpal community!',
       react: WaitlistEmailTemplate()
     });
