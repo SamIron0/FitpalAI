257a52c7b0871fe70ea14d406b65332edf10033a
sam <samuelironkwec@gmail.com>
2024-01-29 15:42:19 -0600

    x

--- pages/api/country.js
+++ pages/api/country.js
@@ -3,6 +3,8 @@ const geoip = require('geoip-lite');
 export default function handler(req, res) {
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const geo = geoip.lookup(ip);
+  console.log('geo',geo);
+  console.log('ip',ip);
   
   if (geo) {
     res.status(200).json({ country: geo.country });
