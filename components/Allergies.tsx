6ea5bb03f5959a4bdc1e45b44db388c3843498e2
sam <samuelironkwec@gmail.com>
2024-01-30 10:47:16 -0600

    x

---
+++ components/Allergies.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Button } from './ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle
+} from './ui/card';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+
+import { Separator } from './ui/separator';
+
+export function CardsShare() {
+  return (
+    <Card>
+      <CardHeader className="pb-3">
+        <CardTitle>Share this document</CardTitle>
+        <CardDescription>
+          Anyone with the link can view this document.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex space-x-2">
+          <Label htmlFor="link" className="sr-only">
+            Link
+          </Label>
+          <Input
+            id="link"
+            value="http://example.com/link/to/document"
+            readOnly
+          />
+          <Button variant="secondary" className="shrink-0">
+            Copy Link
+          </Button>
+        </div>
+        <Separator className="my-4" />
+        <div className="space-y-4">
+          <h4 className="text-sm font-medium">People with access</h4>
+          <div className="grid gap-6">
+            <div className="flex items-center justify-between space-x-4">
+              <div className="flex items-center space-x-4">
+                <p className="text-sm font-medium leading-none">
+                  Olivia Martin
+                </p>
+                <p className="text-sm text-muted-foreground">m@example.com</p>
+              </div>
+              <Button variant="secondary">Remove</Button>
+            </div>
+            <div className="flex items-center justify-between space-x-4">
+              <div className="flex items-center space-x-4">
+                <p className="text-sm font-medium leading-none">
+                  Isabella Nguyen
+                </p>
+                <p className="text-sm text-muted-foreground">b@example.com</p>
+              </div>
+              <Button variant="secondary">Remove</Button>
+            </div>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
