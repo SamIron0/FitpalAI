5bba3d96e4f1d8c129ba084b80903e60a4ebde76
sam <samuelironkwec@gmail.com>
2024-01-30 16:40:40 -0600

    x

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -7,7 +7,7 @@ export default async function Survey() {
     // if user has filled survey display thank you screeen
   }
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center w-full justify-center">
       <SurveyUI user={session?.user} />{' '}
     </div>
   );
