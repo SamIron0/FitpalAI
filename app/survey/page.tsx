c53b0067595f1bc50dc0e585bbd6a9a6e4bd34b1
sam <samuelironkwec@gmail.com>
2024-01-31 14:01:13 -0600

    x

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -1,15 +1,12 @@
+import { redirect } from 'next/navigation';
 import { getSession } from '../supabase-server';
 import { SurveyUI } from './SurveyUI';
 
 export default async function Survey() {
   const session = await getSession();
-
-  if (!session) {
-    window.location.href = '/signin?next=' + window.location.pathname;
-  }
   return (
-    <div className="flex items-center w-full p-4 justify-center">
-      <SurveyUI user={session?.user} />{' '}
+    <div className="flex items-center w-full justify-center">
+      <SurveyUI user={session?.user} />
     </div>
   );
 }
