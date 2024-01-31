5634a9ea63557a7e69cdc354d8b21b10ac89ee94
sam <samuelironkwec@gmail.com>
2024-01-30 21:06:04 -0600

    x

--- app/api/save-survey/route.ts
+++ app/api/save-survey/route.ts
@@ -1,14 +1,20 @@
-
+import { createSurveyResponse } from '@/utils/supabase-admin';
 export async function POST(req: Request) {
-  if (req.method === "POST") {
-    try {}catch (err: any) {
+  if (req.method === 'POST') {
+    try {
+      const { values } = await req.json();
+      const response = await createSurveyResponse(values);
+      return new Response(JSON.stringify(response), {
+        status: 200
+      });
+    } catch (err: any) {
       console.log(err);
       return new Response(JSON.stringify(err), { status: 500 });
     }
   } else {
-    return new Response("Method Not Allowed", {
-      headers: { Allow: "POST" },
-      status: 405,
+    return new Response('Method Not Allowed', {
+      headers: { Allow: 'POST' },
+      status: 405
     });
   }
 }
