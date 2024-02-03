493a5fd22fa4ee1f04f3b583df39ad930cbc96cf
sam <samuelironkwec@gmail.com>
2024-02-03 09:43:10 -0600

    x

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { Card } from '@/components/ui/card';
 import { DashboardUI } from './DashboardUI';
+import { getSession } from '../supabase-server';
 export default async function Pantry() {
+  const  session = await getSession();
   return (
     <div className="flex w-full">
         <DashboardUI />
