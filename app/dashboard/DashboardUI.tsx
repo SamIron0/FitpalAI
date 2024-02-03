921ddaa9eaafa82117219f095a230dba66e5dd62
sam <samuelironkwec@gmail.com>
2024-02-03 11:07:17 -0600

    x

--- app/dashboard/DashboardUI.tsx
+++ app/dashboard/DashboardUI.tsx
@@ -37,7 +37,7 @@ import { postData } from '@/utils/helpers';
 import { Pantry } from '@/components/Pantry';
 import { Calories } from '@/components/Calories';
 import toast from 'react-hot-toast';
-import { User } from 'next-auth';
+import { User } from '@supabase/supabase-js';
 
 const data: Payment[] = [
   {
@@ -176,9 +176,9 @@ export const addAllergies = (allergies: string[]) => {
 };
 
 interface DashboardUIProps {
-  user: User;
+  user: User | undefined;
 }
-export function DashboardUI( { user }: DashboardUIProps) {
+export function DashboardUI({ user }: DashboardUIProps) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
@@ -397,116 +397,225 @@ export function DashboardUI( { user }: DashboardUIProps) {
   const [protein, setProtein] = useState(220);
   const [carbs, setCarbs] = useState(220);
   const [fat, setFat] = useState(220);
-
+  const [activeMealPlans, setActiveMealPlans] = useState(false);
   return (
     <div className="w-full sm:p-12 p-4">
       <div className="w-full pt-8 flex flex-col justify-center md:flex-row">
-        <Card className="w-full md:w-3/5 mb-4 md:mb-0 sm:mr-4">
-          <div className="relative flex flex-1 flex-col">
-            <div className=" p-4 max-w-2xl mx-auto flex flex-col justify-center md:p-6">
-              <div className="flex w-full  flex-col justify-center pb-3">
-                <p className="text-4xl flex justify-center py-10 text-semibold ">
-                  Create a Plan
-                </p>
+        {!activeMealPlans ? (
+          <Card className="w-full md:w-3/5 mb-4 md:mb-0 sm:mr-4">
+            <div className="relative flex flex-1 flex-col">
+              <div className=" p-4 max-w-2xl mx-auto flex flex-col justify-center md:p-6">
+                <div className="flex w-full  flex-col justify-center pb-3">
+                  <p className="text-4xl flex justify-center py-10 text-semibold ">
+                    Create a Plan
+                  </p>
 
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    fetchData(input);
-                  }}
-                >
-                  <div className="relative w-full">
-                    <input
-                      value={input}
-                      disabled={isLoading}
-                      onChange={(e) => setInput(e.target.value)}
-                      className=" px-2 pl-4 w-full h-[60px] focus:outline-none bg-zinc-800 border-[1px] border-zinc-600 text-md rounded-md "
-                      placeholder="Ask about a meal"
-                    />
-                    <button
-                      type="submit"
-                      disabled={isLoading}
-                      className=" absolute end-2.5 bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 inline-flex items-center justify-start pl-2 p7 overflow-hidden  text-blue-500 transition-all duration-150 ease-in-out bg-gray-50 group"
-                    >
-                      <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-500 group-hover:h-full"></span>
-                      <span className="absolute right-0 pr-2 duration-200 ease-out group-hover:translate-x-0">
-                        <svg
-                          className="w-4 h-4 text-black"
-                          fill="none"
-                          stroke="currentColor"
-                          viewBox="0 0 24 24"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M14 5l7 7m0 0l-7 7m7-7H3"
-                          ></path>
-                        </svg>
-                      </span>
-
-                      <span className="relative w-lg pr-3 text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-                        Generate
-                      </span>
-                    </button>
-                  </div>
-                </form>
-              </div>
-              {!isLoading && !completed ? (
-                <div className="pb-24">
-                  <span className="px-2 pr-3 mb-2">Try</span>
-                  <SuggestionPill
-                    onclick={() => {
-                      onPillClick('What should I make for dinner');
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      fetchData(input);
                     }}
-                    icon={'🥘'}
-                    caption="What should I make for dinner "
-                  />
-                  <SuggestionPill
-                    onclick={() => {
-                      onPillClick('Make me a meal plan for the week');
-                    }}
-                    icon={'🍔'}
-                    caption="Make me a meal plan for the week"
-                  />
-                  <SuggestionPill
-                    onclick={() => {
-                      onPillClick('Make me a cheap recipe for lunch');
-                    }}
-                    icon={'🍜'}
-                    caption=" Make me a cheap recipe for lunch"
-                  />
-                </div>
-              ) : (
-                isLoading && !completed && <div className="h-14" />
-              )}
-
-              {isLoading ? (
-                <div className="pt-32">{renderGhostCards()}</div>
-              ) : mealplan?.meals ? (
-                <>
-                  <div className="w-full pb-4 md:px-8 flex flex-row">
-                    <h2 className="w-1/2 flex items-center text-xl ">
-                      Meal Plan
-                    </h2>
-                    <span className="flex w-1/2 items-center justify-end">
-                      <Button
-                        onClick={() => saveMealPlan()}
+                  >
+                    <div className="relative w-full">
+                      <input
+                        value={input}
                         disabled={isLoading}
+                        onChange={(e) => setInput(e.target.value)}
+                        className=" px-2 pl-4 w-full h-[60px] focus:outline-none bg-zinc-800 border-[1px] border-zinc-600 text-md rounded-md "
+                        placeholder="Ask about a meal"
+                      />
+                      <button
+                        type="submit"
+                        disabled={isLoading}
+                        className=" absolute end-2.5 bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 inline-flex items-center justify-start pl-2 p7 overflow-hidden  text-blue-500 transition-all duration-150 ease-in-out bg-gray-50 group"
                       >
-                        Save
-                      </Button>
-                    </span>
+                        <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-500 group-hover:h-full"></span>
+                        <span className="absolute right-0 pr-2 duration-200 ease-out group-hover:translate-x-0">
+                          <svg
+                            className="w-4 h-4 text-black"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M14 5l7 7m0 0l-7 7m7-7H3"
+                            ></path>
+                          </svg>
+                        </span>
+
+                        <span className="relative w-lg pr-3 text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                          Generate
+                        </span>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                {!isLoading && !completed ? (
+                  <div className="pb-24">
+                    <span className="px-2 pr-3 mb-2">Try</span>
+                    <SuggestionPill
+                      onclick={() => {
+                        onPillClick('What should I make for dinner');
+                      }}
+                      icon={'🥘'}
+                      caption="What should I make for dinner "
+                    />
+                    <SuggestionPill
+                      onclick={() => {
+                        onPillClick('Make me a meal plan for the week');
+                      }}
+                      icon={'🍔'}
+                      caption="Make me a meal plan for the week"
+                    />
+                    <SuggestionPill
+                      onclick={() => {
+                        onPillClick('Make me a cheap recipe for lunch');
+                      }}
+                      icon={'🍜'}
+                      caption=" Make me a cheap recipe for lunch"
+                    />
                   </div>
-                  {renderResultBox()}
-                </>
-              ) : (
-                emptyState()
-              )}
+                ) : (
+                  isLoading && !completed && <div className="h-14" />
+                )}
+
+                {isLoading ? (
+                  <div className="pt-32">{renderGhostCards()}</div>
+                ) : mealplan?.meals ? (
+                  <>
+                    <div className="w-full pb-4 md:px-8 flex flex-row">
+                      <h2 className="w-1/2 flex items-center text-xl ">
+                        Meal Plan
+                      </h2>
+                      <span className="flex w-1/2 items-center justify-end">
+                        <Button
+                          onClick={() => saveMealPlan()}
+                          disabled={isLoading}
+                        >
+                          Save
+                        </Button>
+                      </span>
+                    </div>
+                    {renderResultBox()}
+                  </>
+                ) : (
+                  emptyState()
+                )}
+              </div>
             </div>
-          </div>
-        </Card>
+          </Card>
+        ) : (
+          <Card className="w-full md:w-3/5 mb-4 md:mb-0 sm:mr-4">
+            <div className="relative flex flex-1 flex-col">
+              <div className=" p-4 max-w-2xl mx-auto flex flex-col justify-center md:p-6">
+                <div className="flex w-full  flex-col justify-center pb-3">
+                  <p className="text-4xl flex justify-center py-10 text-semibold ">
+                    Create a Plan
+                  </p>
+
+                  <form
+                    onSubmit={(e) => {
+                      e.preventDefault();
+                      fetchData(input);
+                    }}
+                  >
+                    <div className="relative w-full">
+                      <input
+                        value={input}
+                        disabled={isLoading}
+                        onChange={(e) => setInput(e.target.value)}
+                        className=" px-2 pl-4 w-full h-[60px] focus:outline-none bg-zinc-800 border-[1px] border-zinc-600 text-md rounded-md "
+                        placeholder="Ask about a meal"
+                      />
+                      <button
+                        type="submit"
+                        disabled={isLoading}
+                        className=" absolute end-2.5 bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 inline-flex items-center justify-start pl-2 p7 overflow-hidden  text-blue-500 transition-all duration-150 ease-in-out bg-gray-50 group"
+                      >
+                        <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-500 group-hover:h-full"></span>
+                        <span className="absolute right-0 pr-2 duration-200 ease-out group-hover:translate-x-0">
+                          <svg
+                            className="w-4 h-4 text-black"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M14 5l7 7m0 0l-7 7m7-7H3"
+                            ></path>
+                          </svg>
+                        </span>
+
+                        <span className="relative w-lg pr-3 text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                          Generate
+                        </span>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                {!isLoading && !completed ? (
+                  <div className="pb-24">
+                    <span className="px-2 pr-3 mb-2">Try</span>
+                    <SuggestionPill
+                      onclick={() => {
+                        onPillClick('What should I make for dinner');
+                      }}
+                      icon={'🥘'}
+                      caption="What should I make for dinner "
+                    />
+                    <SuggestionPill
+                      onclick={() => {
+                        onPillClick('Make me a meal plan for the week');
+                      }}
+                      icon={'🍔'}
+                      caption="Make me a meal plan for the week"
+                    />
+                    <SuggestionPill
+                      onclick={() => {
+                        onPillClick('Make me a cheap recipe for lunch');
+                      }}
+                      icon={'🍜'}
+                      caption=" Make me a cheap recipe for lunch"
+                    />
+                  </div>
+                ) : (
+                  isLoading && !completed && <div className="h-14" />
+                )}
+
+                {isLoading ? (
+                  <div className="pt-32">{renderGhostCards()}</div>
+                ) : mealplan?.meals ? (
+                  <>
+                    <div className="w-full pb-4 md:px-8 flex flex-row">
+                      <h2 className="w-1/2 flex items-center text-xl ">
+                        Meal Plan
+                      </h2>
+                      <span className="flex w-1/2 items-center justify-end">
+                        <Button
+                          onClick={() => saveMealPlan()}
+                          disabled={isLoading}
+                        >
+                          Save
+                        </Button>
+                      </span>
+                    </div>
+                    {renderResultBox()}
+                  </>
+                ) : (
+                  emptyState()
+                )}
+              </div>
+            </div>
+          </Card>
+        )}
         <Card className="w-full flex justify-center py-4 sm:w-2/5 mb-4 sm:mb-0 sm:mr-4">
           <Calories proteins={protein} fats={fat} carbs={carbs} />
         </Card>
