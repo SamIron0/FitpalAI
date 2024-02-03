838aeff8cd208e7b2351895cd7f23d5f4618c6c3
sam <samuelironkwec@gmail.com>
2024-02-02 20:41:47 -0600

    x

--- components/Calories.tsx
+++ components/Calories.tsx
@@ -2,22 +2,27 @@
 import { Doughnut } from 'react-chartjs-2';
 
 const data = {
-  labels: ["JavaScript", "Python", "Ruby"],
+  labels: ['JavaScript', 'Python', 'Ruby'],
   datasets: [
     {
-      label: "My First Dataset",
+      label: 'My First Dataset',
       data: [300, 50, 100],
       backgroundColor: [
-        "rgb(133, 105, 241)",
-        "rgb(164, 101, 241)",
-        "rgb(101, 143, 241)",
+        'rgb(133, 105, 241)',
+        'rgb(164, 101, 241)',
+        'rgb(101, 143, 241)'
       ],
-      hoverOffset: 4,
-    },
-  ],
+      hoverOffset: 4
+    }
+  ]
 };
 
-function ChartComponent() {
+interface CaloriesProps {
+  protein: number;
+  fat: number;
+  carbs: number;
+}
+function Calories({ protein, fat, carbs }: CaloriesProps) {
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       <div className="py-3 px-5 bg-gray-50">Doughnut chart</div>
@@ -28,4 +33,4 @@ function ChartComponent() {
   );
 }
 
-export default ChartComponent;
+export default Calories;
