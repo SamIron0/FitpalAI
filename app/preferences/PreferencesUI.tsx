096bb43933a19514053bcf54172e8a6a39d0e686
sam <samuelironkwec@gmail.com>
2024-02-04 12:57:57 -0600

    x

--- app/preferences/PreferencesUI.tsx
+++ app/preferences/PreferencesUI.tsx
@@ -35,26 +35,9 @@ const sidebarNavItems = [
 interface PreferencesUIProps {
   children: React.ReactNode;
 }
-export function PreferencesUI( { children }: PreferencesUIProps ) {
+export function PreferencesUI({ children }: PreferencesUIProps) {
   return (
-    <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="space-y-6 p-10 pb-16 md:block">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
           <p className="text-muted-foreground">
@@ -65,9 +48,9 @@ export function PreferencesUI( { children }: PreferencesUIProps ) {
         <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
           <aside className="-mx-4 lg:w-1/5">
             <SidebarNav items={sidebarNavItems} />
-          </aside> <div className="flex-1 lg:max-w-2xl">{children}</div>
+          </aside>{' '}
+          <div className="flex-1 lg:max-w-2xl">{children}</div>
         </div>
       </div>
-    </>
   );
 }
--- app/preferences/page.tsx
+++ app/preferences/page.tsx
@@ -1,11 +1,11 @@
-import {  PreferencesUI } from './PreferencesUI';
+import { PreferencesUI } from './PreferencesUI';
 import { Notifications } from './notifications';
 export default async function Preferences() {
   return (
     <div className="flex w-full">
-        <PreferencesUI  children={ Notifications()}/>
-    
+      <PreferencesUI>
+        <Notifications />
+      </PreferencesUI>
     </div>
-    
   );
 }
