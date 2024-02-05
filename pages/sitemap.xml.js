db6867c19fb2e3e6c7717bcd42a991cc84d970b7
sam <samuelironkwec@gmail.com>
2024-02-05 10:37:04 -0600

    x

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,4 @@
-function generateSiteMap(posts) {
+function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
@@ -20,11 +20,11 @@ function SiteMap() {
 
 export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
+  //const request = await fetch(EXTERNAL_DATA_URL);
+  //const posts = await request.json();
 
   // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSiteMap();
 
   res.setHeader('Content-Type', 'text/xml');
   // we send the XML to the browser
