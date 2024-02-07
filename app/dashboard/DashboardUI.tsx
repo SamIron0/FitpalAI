7b3e660f477cf7eadec7dc1d9c4b3ed3d3807129
sam <samuelironkwec@gmail.com>
2024-02-06 18:27:50 -0600

    x

--- app/dashboard/DashboardUI.tsx
+++ app/dashboard/DashboardUI.tsx
@@ -486,55 +486,40 @@ export function DashboardUI({ user }: DashboardUIProps) {
             <Card className="w-full md:w-3/5 mb-4 md:mb-0 md:mr-4">
               <div className="relative flex flex-1 flex-col">
                 <div className=" p-4 w-full mx-auto flex flex-col justify-center md:p-6">
-                  <div className="flex w-full  flex-col justify-center pb-3">
-                    <p className="text-4xl flex justify-center py-10 text-semibold ">
-                      Create a Plan
-                    </p>
+                  {!createdMealplan?.meals && (
+                    <div className="flex w-full  flex-col justify-center pb-3">
+                      <p className="text-4xl flex justify-center py-10 text-semibold ">
+                        Create a Plan
+                      </p>
 
-                    <form
-                      onSubmit={(e) => {
-                        e.preventDefault();
-                        fetchData(input);
-                      }}
-                    >
-                      <div className="relative w-full">
-                        <input
-                          value={input}
-                          disabled={isLoading}
-                          onChange={(e) => setInput(e.target.value)}
-                          className=" px-2 pl-4 w-full h-[60px] focus:outline-none bg-zinc-800 border-[1px] border-zinc-600 text-md rounded-md "
-                          placeholder="Ask about a meal"
-                        />
-                        <button
-                          type="submit"
-                          disabled={isLoading}
-                          className=" absolute end-2.5 bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 inline-flex items-center justify-start pl-2 p7 overflow-hidden  text-blue-500 transition-all duration-150 ease-in-out bg-gray-50 group"
-                        >
-                          <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-500 group-hover:h-full"></span>
-                          <span className="absolute right-0 pr-2 duration-200 ease-out group-hover:translate-x-0">
-                            <svg
-                              className="w-4 h-4 text-black"
-                              fill="none"
-                              stroke="currentColor"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M14 5l7 7m0 0l-7 7m7-7H3"
-                              ></path>
-                            </svg>
-                          </span>
-
-                          <span className="relative w-lg pr-3 text-left transition-colors duration-200 ease-in-out group-hover:text-white">
-                            Generate
-                          </span>
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+                      <form
+                        onSubmit={(e) => {
+                          e.preventDefault();
+                          fetchData(input);
+                        }}
+                      >
+                        <div className="relative w-full">
+                          <input
+                            value={input}
+                            disabled={isLoading}
+                            onChange={(e) => setInput(e.target.value)}
+                            className=" px-2 pl-4 w-full h-[60px] focus:outline-none bg-zinc-800 border-[1px] border-zinc-600 text-md rounded-md "
+                            placeholder="Ask about a meal"
+                          />
+                          <button
+                            type="submit"
+                            disabled={isLoading}
+                            className=" absolute end-2.5 bottom-2.5 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 inline-flex items-center justify-start pl-2 p7 overflow-hidden  text-blue-500 transition-all duration-150 ease-in-out bg-gray-50 group"
+                          >
+                            <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-blue-500 group-hover:h-full"></span>
+                            <span className="relative w-lg pr-3 text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+                              Generate
+                            </span>
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                  )}
                   {!isLoading && !completed ? (
                     <div className="pb-24">
                       <span className="px-2 pr-3 mb-2">Try</span>
@@ -568,6 +553,7 @@ export function DashboardUI({ user }: DashboardUIProps) {
                     <div className="pt-24">{renderGhostCards()}</div>
                   ) : createdMealplan?.meals ? (
                     <>
+                      <p className='text-2xl'>{input}</p>
                       <div className="w-full pb-4 flex flex-row">
                         <h2 className="w-1/2 flex items-center text-xl ">
                           Meal Plan
