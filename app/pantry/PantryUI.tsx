294b22525c2acaa1ad205f48ef53036e4bf7dec4
sam <samuelironkwec@gmail.com>
2024-02-08 10:27:54 -0600

    x

--- app/pantry/PantryUI.tsx
+++ app/pantry/PantryUI.tsx
@@ -6,14 +6,10 @@ export default function PantryUI() {
   return (
     <div className="space-y-6 p-6 sm:p-12 pt-20  pb-16 flex flex-col ">
       <div className="space-y-0.5 flex flex-col">
-        <h2 className="text-2xl font-bold tracking-tight">Preferences</h2>
-        <p className="text-muted-foreground">
-          Manage your account preferences and customize your experience
-        </p>
+        <h2 className="text-2xl font-bold tracking-tight">Pantry</h2>
       </div>
       <Separator className="mt-6" />
 
-      <h1 className="pb-5 pt-12 text-3xl"> PANTRY</h1>
       <Card className="w-full max-w-3xl">
         <EmptyPantry />
       </Card>
