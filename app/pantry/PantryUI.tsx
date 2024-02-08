125ebb47b8e93460c68c16c8a2506d0c5448050d
sam <samuelironkwec@gmail.com>
2024-02-08 10:12:25 -0600

    x

--- app/pantry/PantryUI.tsx
+++ app/pantry/PantryUI.tsx
@@ -3,7 +3,7 @@ import { Card } from '@/components/ui/card';
 
 export default function PantryUI() {
   return (
-    <div className="w-full h-full p-4 flex-row flex justify-center ">
+    <div className="w-full h-full p-4 flex-col flex justify-center ">
       <h1 className="pb-5 pt-12 text-3xl"> PANTRY</h1>
       <Card className="w-full max-w-3xl">
         <EmptyPantry />
