911f8d373951c97e1ce8b7f91cea12da17f6a95c
sam <samuelironkwec@gmail.com>
2024-02-08 10:53:48 -0600

    x

--- app/pantry/PantryUI.tsx
+++ app/pantry/PantryUI.tsx
@@ -4,13 +4,13 @@ import { Separator } from '@/components/ui/separator';
 
 export default function PantryUI() {
   return (
-    <div className="space-y-6 p-6 sm:p-12 pt-20  pb-16 flex flex-col ">
+    <div className="space-y-6 p-6 sm:p-12 pt-20  pb-16 flex flex-col  w-full ">
       <div className="space-y-0.5 flex flex-col">
         <h2 className="text-2xl font-bold tracking-tight">Pantry</h2>
       </div>
       <Separator className="mt-6" />
 
-      <Card className="w-full max-w-3xl">
+      <Card className="w-full mx-auto max-w-4xl">
         <EmptyPantry />
       </Card>
     </div>
