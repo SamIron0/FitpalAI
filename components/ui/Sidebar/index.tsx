e1c0f7cab417ee33800b55a2e346d3c436afc088
sam <samuelironkwec@gmail.com>
2024-02-08 11:52:28 -0600

    x

--- components/ui/Sidebar/index.tsx
+++ components/ui/Sidebar/index.tsx
@@ -48,85 +48,79 @@ const Sidebar = ({ session }: SidebarProps) => {
             <div className="h-screen ">
               <aside className="flex flex-col h-full items-center w-16  overflow-y-auto border-r rtl:border-l rtl:border-r-0 bg-black border-black">
                 <nav className="flex flex-col flex-1 space-y-6">
-                  <div className="flex flex-col h-full justify-between ">
-                    <div>
-                      <button
-                        className="p-2 pt-2 flex flex-col items-center w-full"
-                        onClick={() => setSidebarOpen(!isSidebarOpen)}
-                      >
-                        <BiMenuAltLeft className="w-8 h-8 text-gray-200" />
-                      </button>
+                  <button
+                    className="p-2 pt-2 flex flex-col items-center w-full"
+                    onClick={() => setSidebarOpen(!isSidebarOpen)}
+                  >
+                    <BiMenuAltLeft className="w-8 h-8 text-gray-200" />
+                  </button>
 
-                      <a href={disabled ? '#' : '/dashboard'}>
+                  <a href={disabled ? '#' : '/dashboard'}>
+                    <img
+                      className="w-[37px] h-[37px]"
+                      src="/logo-2.svg"
+                      alt=""
+                    />
+                  </a>
+
+                  <a
+                    href={disabled ? '#' : '/dashboard'}
+                    className="p-1.5 text-gray-200 flex justify-center focus:outline-nones transition-colors duration-200 rounded-lg hover:bg-zinc-800 "
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                      />
+                    </svg>
+                  </a>
+
+                  <a
+                    href={disabled ? '#' : '/pantry'}
+                    className="p-1.5 flex justify-center focus:outline-nones transition-colors duration-200 rounded-lg text-gray-200 hover:bg-zinc-800 "
+                  >
+                    <TbFridge
+                      className="w-6 h-6 hover:text-gray-200 "
+                      color="white"
+                    />
+                  </a>
+
+                  <a
+                    href={disabled ? '#' : '/preferences'}
+                    className="p-1.5 text-gray-200 flex justify-center focus:outline-nones transition-colors duration-200 rounded-lg  hover:bg-zinc-800 "
+                  >
+                    <TiHeartOutline
+                      className="w-6 h-6 hover:text-gray-200 "
+                      color="white"
+                    />
+                  </a>
+                </nav>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <div className="flex flex-col ">
+                      <a className="flex justify-center " href="#">
                         <img
-                          className="w-[37px] h-[37px]"
-                          src="/logo-2.svg"
+                          className="object-cover w-8 h-8 rounded-full"
+                          src="/user-01.png"
                           alt=""
                         />
                       </a>
-
-                      <a
-                        href={disabled ? '#' : '/dashboard'}
-                        className="p-1.5 text-gray-200 flex justify-center focus:outline-nones transition-colors duration-200 rounded-lg hover:bg-zinc-800 "
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                          />
-                        </svg>
-                      </a>
-
-                      <a
-                        href={disabled ? '#' : '/pantry'}
-                        className="p-1.5 flex justify-center focus:outline-nones transition-colors duration-200 rounded-lg text-gray-200 hover:bg-zinc-800 "
-                      >
-                        <TbFridge
-                          className="w-6 h-6 hover:text-gray-200 "
-                          color="white"
-                        />
-                      </a>
-
-                      <a
-                        href={disabled ? '#' : '/preferences'}
-                        className="p-1.5 text-gray-200 flex justify-center focus:outline-nones transition-colors duration-200 rounded-lg  hover:bg-zinc-800 "
-                      >
-                        <TiHeartOutline
-                          className="w-6 h-6 hover:text-gray-200 "
-                          color="white"
-                        />
-                      </a>
                     </div>
-                    <div>
-                      <Popover>
-                        <PopoverTrigger asChild>
-                          <div className="flex flex-col ">
-                            <a className="flex justify-center " href="#">
-                              <img
-                                className="object-cover w-8 h-8 rounded-full"
-                                src="/user-01.png"
-                                alt=""
-                              />
-                            </a>
-                          </div>
-                        </PopoverTrigger>
-                        <PopoverContent>
-                          <Button onClick={() => signOut()} variant="outline">
-                            Logout
-                          </Button>
-                        </PopoverContent>
-                      </Popover>
-                    </div>
-                  </div>
-                </nav>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <Button onClick={() => signOut()} variant="outline">
+                      Logout
+                    </Button>
+                  </PopoverContent>
+                </Popover>
               </aside>
             </div>
           )}
