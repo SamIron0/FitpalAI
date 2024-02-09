c0c45f3d87df24a56a89b817ba677b80ae0d6713
sam <samuelironkwec@gmail.com>
2024-02-09 17:02:54 -0600

    x

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,7 +85,7 @@ function Announcement() {
           plans.
         </p>
         <a
-          href="#"
+          href="/preferences"
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
           Go now <span aria-hidden="true">&rarr;</span>
